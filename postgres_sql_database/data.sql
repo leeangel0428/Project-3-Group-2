78356a61e5bcd8ede86cab5c087d87acfe70ff27
leeangel0428 <leeangel0428@gmail.com>
2023-10-17 17:57:48 -0500

    fixed sql file and schema

--- postgres_sql_database/data.sql
+++ postgres_sql_database/data.sql
@@ -1,56 +1,56 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/ then edited by Angel L. to fix "no unique constraint error".
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "annual_family_income" (
-    "Year_Quarter" INT   NOT NULL,
-    "US_Household_Mean_Income" INT   NOT NULL,
-    "Midwestern_Household_Mean_Income" INT   NOT NULL,
-    "Northeastern_Household_Mean_Income" INT   NOT NULL,
-    "Southern_Household_Mean_Income" INT   NOT NULL,
-    "Western_Household_Mean_Income" INT   NOT NULL,
+    "year_quarter" INT   NOT NULL,
+    "us_household_mean_income" INT   NOT NULL,
+    "midwestern_household_mean_income" INT   NOT NULL,
+    "northeastern_household_mean_income" INT   NOT NULL,
+    "southern_household_mean_income" INT   NOT NULL,
+    "western_household_mean_income" INT   NOT NULL,
     CONSTRAINT "pk_annual_family_income" PRIMARY KEY (
-        "Year_Quarter"
+        "year_quarter"
      )
 );
 
 CREATE TABLE "annual_house_price" (
-    "Year_Quarter" INT   NOT NULL,
-    "Southern_Average_Price" INT   NOT NULL,
-    "Western_Average_Price" INT   NOT NULL,
-    "Nationwide_Average_Price" INT   NOT NULL,
-    "Northeastern_Average_Price" INT   NOT NULL,
-    "Midwestern_Average_Price" INT   NOT NULL
+    "year_quarter" INT   NOT NULL,
+    "southern_average_price" INT   NOT NULL,
+    "western_average_price" INT   NOT NULL,
+    "nationwide_average_price" INT   NOT NULL,
+    "northeastern_average_price" INT   NOT NULL,
+    "midwestern_average_price" INT   NOT NULL
 );
 
 CREATE TABLE "annual_interest_rate" (
-    "Year_Quarter" INT   NOT NULL,
-    "Fixed_30_year_Rate" FLOAT   NOT NULL
+    "year_quarter" INT   NOT NULL,
+    "fixed_30_year_rate" FLOAT   NOT NULL
 );
 
 CREATE TABLE "quarterly_house_price" (
-    "Year_Quarter" INT   NOT NULL,
-    "Southern_Average_Price" INT   NOT NULL,
-    "Western_Average_Price" INT   NOT NULL,
-    "Nationwide_Average_Price" INT   NOT NULL,
-    "Northeastern_Average_Price" INT   NOT NULL,
-    "Midwestern_Average_Price" INT   NOT NULL
+    "year_quarter" INT   NOT NULL,
+    "southern_average_price" INT   NOT NULL,
+    "western_average_price" INT   NOT NULL,
+    "nationwide_average_price" INT   NOT NULL,
+    "northeastern_average_price" INT   NOT NULL,
+    "midwestern_average_price" INT   NOT NULL
 );
 
 CREATE TABLE "quarterly_interest_rate" (
-    "Year_Quarter" INT   NOT NULL,
-    "Fixed_30_year_Rate" FLOAT   NOT NULL
+    "year_quarter" INT   NOT NULL,
+    "fixed_30_year_rate" FLOAT   NOT NULL
 );
 
-ALTER TABLE "annual_house_price" ADD CONSTRAINT "fk_annual_house_price_Year_Quarter" FOREIGN KEY("Year_Quarter")
-REFERENCES "annual_family_income" ("Year_Quarter");
+ALTER TABLE "annual_house_price" ADD CONSTRAINT "fk_annual_house_price_year_quarter" FOREIGN KEY("year_quarter")
+REFERENCES "annual_family_income" ("year_quarter");
 
-ALTER TABLE "annual_interest_rate" ADD CONSTRAINT "fk_annual_interest_rate_Year_Quarter" FOREIGN KEY("Year_Quarter")
-REFERENCES "annual_family_income" ("Year_Quarter");
+ALTER TABLE "annual_interest_rate" ADD CONSTRAINT "fk_annual_interest_rate_year_quarter" FOREIGN KEY("year_quarter")
+REFERENCES "annual_family_income" ("year_quarter");
 
-ALTER TABLE "quarterly_house_price" ADD CONSTRAINT "fk_quarterly_house_price_Year_Quarter" FOREIGN KEY("Year_Quarter")
-REFERENCES "annual_house_price" ("Year_Quarter");
+ALTER TABLE "quarterly_house_price" ADD CONSTRAINT "fk_quarterly_house_price_year_quarter" FOREIGN KEY("year_quarter")
+REFERENCES "annual_family_income" ("year_quarter");
 
-ALTER TABLE "quarterly_interest_rate" ADD CONSTRAINT "fk_quarterly_interest_rate_Year_Quarter" FOREIGN KEY("Year_Quarter")
-REFERENCES "annual_interest_rate" ("Year_Quarter");
+ALTER TABLE "quarterly_interest_rate" ADD CONSTRAINT "fk_quarterly_interest_rate_year_quarter" FOREIGN KEY("year_quarter")
+REFERENCES "annual_family_income" ("year_quarter");
 
